75a8ceffd9471088cd6d0af958edc60ee061428c
Liu Lantao <Lax@users.noreply.github.com>
2017-06-02 09:16:53 +0800

    Create duang.rb

---
+++ templates/duang.rb
@@ -0,0 +1,38 @@
+gem_group :development, :test do
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
+end
+
+gem_group :development, :test do
+  gem "factory_girl_rails"
+  gem "pry-byebug"
+  gem "pry-rails"
+  gem "rspec-rails"
+end
+
+gem_group :test do
+  gem 'rails-controller-testing'
+end
+
+gem 'haml-rails'
+
+after_bundle do
+
+  generate "rspec:install"
+
+  file 'spec/__include_spec.rb', <<-CODE
+require 'rails_helper'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+CODE
+
+  file 'spec/support/factory_girl.rb', <<-CODE
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
+CODE
+
+  #rails_command "haml:erb2haml"
+  #ENV['DATABASE_URL']='sqlite3::memory:'
+  #rails_command "db:create db:migrate"
+
+end	# end after_bundle
