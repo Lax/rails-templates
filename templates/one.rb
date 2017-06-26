f9f2d70868fb81180d8b7183cebda5e2c25f60db
Liu Lantao <liulantao@gmail.com>
2017-06-27 00:12:53 +0800

    layout
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- templates/one.rb
+++ templates/one.rb
@@ -63,22 +63,117 @@ gem_group :development, :staging do
   gem 'rack-mini-profiler', require: false
 end
 
-#========== Landing ==========#
-after_bundle do
-  inside('app/views/pages/') do
-    create_file 'landing.html.haml', %Q{.d-flex.justify-content-center<>
-  %h1 Think different.}
+#========== Layout ==========#
+inside 'app/views/layouts/' do
+  gsub_file 'application.html.erb', '= yield', %!= render 'layouts/body'!
+  insert_into_file 'application.html.erb', %!    <%= stylesheet_link_tag    'http://blog.liulantao.com/iconfont/iconfont/material-icons.css', media: 'all', 'data-turbolinks-track': 'reload' %>\n!, after: %!<%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track': 'reload' %>\n!
+
+  file '_body.html.haml' do
+    <<-CODE
+= render 'layouts/header'
+= render 'layouts/main'
+= render 'layouts/footer'
+CODE
   end
 
+  file '_header.html.haml' do
+    <<-CODE
+= render 'layouts/menu'
+CODE
+  end
+
+  file '_main.html.haml' do
+    <<-CODE
+%main
+  .container<
+    = render 'layouts/flash'
+  .container<
+    = content_for?(:content) ? yield(:content) : yield
+CODE
+  end
+
+  file '_footer.html.haml' do
+    <<-CODE
+%footer.footer.text-muted
+  .container
+    .list-inline
+      %a.m-2{href: '/'}<>= t('menu.home')
+      \|
+      %a.m-2{href: '/'}<>= t('menu.support')
+
+    %p<
+      .small<
+        = surround " 由", " 提供技术支持" do
+          %a<> Lax
+CODE
+  end
+
+  file '_flash.html.haml' do
+    <<-CODE
+- flash.each do |name, msg|
+  %div{class: flash_class(name, [:flash, :'alert-dismissible']), role: :alert}
+    %button.close{type: "button", "data-dismiss": "alert", "aria-label": "Close"}
+      %span{"aria-hidden": true} &times;
+    %strong= '[%s]' % name
+    %span= msg
+CODE
+  end
+
+  file '_menu.html.haml' do
+    <<-CODE
+%nav.navbar.navbar-toggleable-sm.navbar-light.bg-faded
+  %button.navbar-toggler.navbar-toggler-left.navbar-toggler-right{'aria-controls': 'navbarNavCollapse', 'aria-expanded': 'false', 'aria-label': 'Toggle navigation', 'data-target': '#navbarNavCollapse', 'data-toggle': 'collapse', type: 'button'}
+    %span.navbar-toggler-icon
+
+  %a.navbar-brand{href: '#'}<
+    %img{alt: :🏝⛵️🍀🌿}
+
+  #navbarNavCollapse.collapse.navbar-collapse
+    .navbar-nav.mr-auto
+      = link_to :root, class: active_class(root_path, base: 'nav-item nav-link') do
+        %i.material-icons.md-18<> home
+        = t('menu.home')
+        %span.sr-only> (current)
+      = content_for?(:controller_menu) ? yield(:controller_menu) : ''
+
+    .navbar-nav
+      = link_to t('menu.login'), :root, class: active_class(root_path, base: 'nav-item nav-link')
+      .nav-item.dropdown
+        %a#navbarProfileMenuLink.nav-link.dropdown-toggle{"aria-expanded": "false", "aria-haspopup": "true", "data-toggle": "dropdown"}
+          %i.material-icons.md-18<> account_box
+          = t('menu.profile')
+          %span.caret>
+        .dropdown-menu.dropdown-menu-right{"aria-labelledby": "navbarProfileMenuLink"}
+          %h6.dropdown-header<
+            Me
+            %br<
+            = precede "@" do
+              %b>= "<login>"
+          .dropdown-divider
+          = link_to t('menu.edit_profile'), :root, class: active_class(root_path, base: 'dropdown-item')
+CODE
+  end
+end
+
+#========== Landing ==========#
+after_bundle do
   generate :controller, :pages, :landing, '--skip', '--no-helper-specs'
   route %q{root to: 'pages#landing'}
 
+  inside('app/views/pages/') do
+    file 'landing.html.haml', <<-CODE
+.d-flex.justify-content-center<>
+  %h1 Think different.
+CODE
+  end
+
   inside('spec/views/pages/') do
-    gsub_file 'landing.html.haml_spec.rb', /^\s.pending .*\n/, %q{  it 'renders landing' do
+    gsub_file 'landing.html.haml_spec.rb', /^\s.pending .*\n/, <<-CODE
+  it 'renders landing' do
     render
     assert_select 'h1'
   end
-}
+CODE
   end
 end
 
@@ -97,11 +192,11 @@ inside('app/assets/stylesheets') do
   gsub_file 'application.scss', /^\s.*= require_tree \.\n/, ''
   gsub_file 'application.scss', /^\s.*= require_self\n/, ''
 
-  append_to_file 'application.scss', %Q{
+  append_to_file 'application.scss', <<-CODE
 @import '#{bs_theme}/variables';
 @import '#{bs_theme}/bootswatch';
 @import 'bootstrap';
-}
+CODE
 end
 
 inside('app/assets/javascripts') do
@@ -126,8 +221,8 @@ end
 
 #========== Helpers ==========#
 inside('app/helpers/') do
-  insert_into_file 'application_helper.rb', after: 'module ApplicationHelper' do
-    %Q{
+  insert_into_file 'application_helper.rb', after: %/module ApplicationHelper\n/ do
+    <<-CODE
   def active_class(link_path, base: '')
     append_class(link_path, append: 'active', base: base)
   end
@@ -146,7 +241,7 @@ inside('app/helpers/') do
     end
     return cls + default
   end
-}
+CODE
   end
 end
 
